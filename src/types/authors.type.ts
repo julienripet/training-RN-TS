c6abda417b00dd06e46f62bf3103c7bc033650a9
Julien Ripet <julien.ripet@derniercri.io>
2022-05-03 17:37:01 +0200

    ADD: basic types

---
+++ src/types/authors.type.ts
@@ -0,0 +1,5 @@
+export type Author = {
+  readonly id: Number;
+  lastname: String;
+  firstname: String;
+};
---
+++ src/types/books.type.ts
@@ -0,0 +1,6 @@
+export type Book = {
+  readonly id: Number;
+  authorId: Number;
+  title: String;
+  year: String;
+};
